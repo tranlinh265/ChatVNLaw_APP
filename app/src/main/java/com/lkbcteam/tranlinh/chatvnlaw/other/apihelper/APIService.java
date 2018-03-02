128608250b389ffd31b17c236dafaa7705733eaf
TranLinh <tranlinh265@gmail.com>
2018-03-03 02:07:52 +0700

    add page filed

--- app/src/main/java/com/lkbcteam/tranlinh/chatvnlaw/other/apihelper/APIService.java
+++ app/src/main/java/com/lkbcteam/tranlinh/chatvnlaw/other/apihelper/APIService.java
@@ -17,7 +17,7 @@ import retrofit2.http.Query;
 
 public interface APIService {
     @GET(Define.Api.SEARCH_LAWYER)
-    Call<SearchLawyerResponse> searchLawyer( @Query("query") @Nullable String name);
+    Call<SearchLawyerResponse> searchLawyer( @Query("query") @Nullable String name, @Query("page") int page);
     @GET(Define.Api.LAWYER_NAME)
     Call<LawyerNameResponse> getAllNameOfLawyer();
     @GET(Define.Api.TOP_LAWYERS)
