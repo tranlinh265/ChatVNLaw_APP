c7f9cf4c08b516b388e07d065d8e92a64120dc8e
TranLinh <tranlinh265@gmail.com>
2018-04-05 20:14:17 +0700

    modify new design

--- app/src/main/java/com/lkbcteam/tranlinh/chatvnlaw/adapter/ChatContentAdapter.java
+++ app/src/main/java/com/lkbcteam/tranlinh/chatvnlaw/adapter/ChatContentAdapter.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.util.TimeUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ import com.lkbcteam.tranlinh.chatvnlaw.view.fragment.BaseFragment;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by tranlinh on 29/01/2018.
@@ -57,10 +59,10 @@ public class ChatContentAdapter extends RecyclerView.Adapter<ChatContentAdapter.
                 v =LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message_time,parent,false);
                 break;
             case 0:
-                v =LayoutInflater.from(parent.getContext()).inflate(R.layout.item_receiver_messages,parent,false);
+                v =LayoutInflater.from(parent.getContext()).inflate(R.layout.item_receiver_messages_new_design,parent,false);
                 break;
             case 2:
-                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_sender_messages,parent,false);
+                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_sender_messages_new_design,parent,false);
                 break;
         }
         ViewHolder vh = new ViewHolder(v,viewType);
@@ -72,7 +74,7 @@ public class ChatContentAdapter extends RecyclerView.Adapter<ChatContentAdapter.
 
         final Message message = mMessageList.get(position);
         if(message != null){
-            if(message.getmMessageId() == null){
+            if(holder.getViewType() == -1){
                 Time time = message.getmMessageInfo().getTime();
                 holder.tvTimeDay.setText(String.format(mContext.getString(R.string.date_format),time.getDay(),time.getMonth(),time.getYear()));
                 return;
@@ -83,38 +85,68 @@ public class ChatContentAdapter extends RecyclerView.Adapter<ChatContentAdapter.
             }
             if(message.getmMessageInfo() != null){
                 if(message.getmMessageInfo().getContentType() != null){
-                    holder.mTextContent.setVisibility(View.GONE);
-                    if(message.getmMessageInfo().getContentType().contains("image/")){
-                        holder.mIvImageContent.setVisibility(View.VISIBLE);
-                        holder.mFileUrl.setVisibility(View.GONE);
-                        Picasso.with(mContext).load(String.valueOf(message.getmMessageInfo().getDownloadURL())).resize(700,(700*message.getmMessageInfo().getHeight()/ message.getmMessageInfo().getWidth())).centerCrop().placeholder(R.drawable.spinning_loading_icon).into(holder.mIvImageContent);
-
-                    }else{
-                        holder.mFileUrl.setVisibility(View.VISIBLE);
-                        holder.mIvImageContent.setVisibility(View.GONE);
-                        holder.mFileUrl.setPaintFlags(holder.mFileUrl.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-                        holder.mFileUrl.setText(message.getmMessageInfo().getName());
-                        holder.mFileUrl.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-//                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(message.getmMessageInfo().getDownloadURL()));
-//                            mContext.startActivity(browserIntent);
-                                showAlertDialog(message.getmMessageInfo());
-//                            mBaseFragment.goNextFragment(FragmentFileContent.newInstance(message.getmMessageInfo().getDownloadURL(), message.getmMessageInfo().getName()),true);
-//                            DownLoadFile.DownLoadFileViaUrl(mContext,message.getmMessageInfo().getName(), message.getmMessageInfo().getDownloadURL());
-                            }
-                        });
-
-                    }
+//                    holder.mTextContent.setVisibility(View.GONE);
+//                    if(message.getmMessageInfo().getContentType().contains("image/")){
+//                        holder.mIvImageContent.setVisibility(View.VISIBLE);
+//                        holder.mFileUrl.setVisibility(View.GONE);
+//                        Picasso.with(mContext).load(String.valueOf(message.getmMessageInfo().getDownloadURL())).resize(700,(700*message.getmMessageInfo().getHeight()/ message.getmMessageInfo().getWidth())).centerCrop().placeholder(R.drawable.spinning_loading_icon).into(holder.mIvImageContent);
+//
+//                    }else{
+//                        holder.mFileUrl.setVisibility(View.VISIBLE);
+//                        holder.mIvImageContent.setVisibility(View.GONE);
+//                        holder.mFileUrl.setPaintFlags(holder.mFileUrl.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+//                        holder.mFileUrl.setText(message.getmMessageInfo().getName());
+//                        holder.mFileUrl.setOnClickListener(view -> {
+////                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(message.getmMessageInfo().getDownloadURL()));
+////                            mContext.startActivity(browserIntent);
+//                            showAlertDialog(message.getmMessageInfo());
+////                            mBaseFragment.goNextFragment(FragmentFileContent.newInstance(message.getmMessageInfo().getDownloadURL(), message.getmMessageInfo().getName()),true);
+////                            DownLoadFile.DownLoadFileViaUrl(mContext,message.getmMessageInfo().getName(), message.getmMessageInfo().getDownloadURL());
+//                        });
+//
+//                    }
                 }else{
                     holder.mTextContent.setVisibility(View.VISIBLE);
                     holder.mTextContent.setText(message.getmMessageInfo().getContent());
-                    holder.mFileUrl.setVisibility(View.GONE);
-                    holder.mIvImageContent.setVisibility(View.GONE);
+//                    holder.mFileUrl.setVisibility(View.GONE);
+//                    holder.mIvImageContent.setVisibility(View.GONE);
 
                 }
                 Time time = message.getmMessageInfo().getTime();
-                holder.mTextTimeStamp.setText(time.getHour()+":"+time.getMinute());
+                String str = time.getHour()+":"+time.getMinute();
+
+                if(holder.getViewType() == 2){
+                    if(message.getmSenderUser() != null){
+                        str = message.getmSenderUser().getDisplayName()+ "," + str;
+                    }
+                }
+                holder.mTextTimeStamp.setText(str);
+
+                if (position == 0) {
+                    if (holder.getViewType() ==2)
+                        holder.mIvSenderPicture.setVisibility(View.VISIBLE);
+                    return;
+                }
+                if (holder.getViewType() == 0 && mMessageList.get(position -1).getIsCurrentUser()){
+                    int duration = (int) (TimeUnit.MILLISECONDS.toMinutes(Long.parseLong(message.getmMessageInfo().getMsgTimeStamp())) -
+                                                TimeUnit.MILLISECONDS.toMinutes(Long.parseLong(mMessageList.get(position -1).getmMessageInfo().getMsgTimeStamp())));
+                    if(duration <= 5){
+                        holder.mTextTimeStamp.setVisibility(View.GONE);
+                        holder.mLayoutContainer.setBackgroundResource(R.drawable.chat_content_bg_1010);
+                    }
+
+                    return;
+                }
+                if (holder.getViewType() == 2 && !mMessageList.get(position -1).getIsCurrentUser()){
+                    int duration = (int) (TimeUnit.MILLISECONDS.toMinutes(Long.parseLong(message.getmMessageInfo().getMsgTimeStamp())) -
+                            TimeUnit.MILLISECONDS.toMinutes(Long.parseLong(mMessageList.get(position -1).getmMessageInfo().getMsgTimeStamp())));
+                    if(duration <= 5){
+                        holder.mIvSenderPicture.setVisibility(View.INVISIBLE);
+                        holder.mTextTimeStamp.setVisibility(View.GONE);
+                        holder.mLayoutContainer.setBackgroundResource(R.drawable.chat_content_bg_0101);
+                    }
+                    return;
+                }
             }
         }
     }
@@ -145,9 +177,11 @@ public class ChatContentAdapter extends RecyclerView.Adapter<ChatContentAdapter.
         private TextView mTextContent, mTextTimeStamp, mFileUrl;
         private ImageView mIvSenderPicture, mIvImageContent;
         private TextView tvTimeDay;
+        private int viewType;
 
         public ViewHolder(View itemView, int viewType) {
             super(itemView);
+            setViewType(viewType);
             if(viewType == -1){
                 tvTimeDay = itemView.findViewById(R.id.tv_time_day);
             }else{
@@ -155,9 +189,17 @@ public class ChatContentAdapter extends RecyclerView.Adapter<ChatContentAdapter.
                 mTextContent = itemView.findViewById(R.id.tv_text_content);
                 mTextTimeStamp = itemView.findViewById(R.id.tv_text_time_stamp);
                 mIvSenderPicture = itemView.findViewById(R.id.iv_sender_picture);
-                mFileUrl = itemView.findViewById(R.id.tv_file_url);
-                mIvImageContent = itemView.findViewById(R.id.iv_image_content);
+//                mFileUrl = itemView.findViewById(R.id.tv_file_url);
+//                mIvImageContent = itemView.findViewById(R.id.iv_image_content);
             }
+        }
+
+        public int getViewType() {
+            return viewType;
+        }
+
+        public void setViewType(int viewType) {
+            this.viewType = viewType;
         }
     }
 }
